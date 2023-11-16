45d5892ea2de88f5b3b1b4d237aa8a65041b6b54
Alex Martins <alexmdebarros@gmail.com>
2023-11-16 16:25:24 -0400

    first commit

---
+++ operacoes.py
@@ -0,0 +1,22 @@
+import math
+
+def soma (a, b):
+    return a + b
+
+def subtracao (a, b):
+    return a - b
+
+def multiplicacao (a, b):
+    return a * b
+
+def divisao (a, b):
+    return a / b
+
+def potenciacao (a, b):
+    return a ** b
+
+def raiz_quadrada(a):
+    return match.sqrt(a)
+
+def logaritmo(a):
+    return match.log(a)
